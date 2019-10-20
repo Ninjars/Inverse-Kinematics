ca952786a15fce5f0536a37ae795c9236ab2bbc2
jez <jeremy+github@jeremystevens.net>
2019-10-20 15:16:13 +0100

    repackaged scripts

---
+++ Assets/Scripts/Common/KillPlane.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlane : MonoBehaviour {
+    private void OnCollisionEnter(Collision other) {
+        other.gameObject.SetActive(false);
+        Debug.Log("Target Missed");
+    }
+}
